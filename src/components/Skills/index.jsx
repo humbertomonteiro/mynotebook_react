a3903ebd0d18cec9ff8ced576692edd1192e59ac
Humberto Monteiro <humbertoaraujox@icloud.com>
2023-02-25 14:40:17 -0300

    att

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,8 +1,5 @@
 import './Skills.css'
 
-import { SiJavascript, SiNodedotjs, SiReact } from 'react-icons/si'
-import { FaGitAlt } from 'react-icons/fa'
-
 export default function Skills() {
     return (
         <section className='container-skills'>
@@ -10,22 +7,22 @@ export default function Skills() {
             <div className='skills'>
                 <h2>Skills</h2>
                 <div className='tecs'>
-                    <div className='tec'>
-                        <h3><SiJavascript className='js'/></h3>
-                        <button>JavaScript</button>
-                    </div>
-                    <div className='tec'>
-                        <h3><FaGitAlt className='git'/></h3>
+                    <a href='/anotacoes' className='tec git'>
+                        <h3><i className="fa-brands fa-git-alt"></i></h3>
                         <button>Git</button>
-                    </div>
-                    <div className='tec'>
-                        <h3><SiReact className='react'/></h3>
-                        <button>React.js</button>
-                    </div>
-                    <div className='tec'>
-                        <h3><SiNodedotjs className='node'/></h3>
+                    </a>
+                    <a href='/anotacoes' className='tec github'>
+                        <h3><i className="fa-brands fa-github"></i></h3>
+                        <button>GitHub</button>
+                    </a>
+                    <a href='/anotacoes' className='tec node'>
+                        <h3><i className="fa-brands fa-node-js"></i></h3>
                         <button>Node.js</button>
-                    </div>
+                    </a>
+                    <a href='/anotacoes' className='tec'>
+                        <h3><i className="fa-brands fa-bootstrap"></i></h3>
+                        <button>Bootstrap</button>
+                    </a>
                 </div>
             </div>
                 
